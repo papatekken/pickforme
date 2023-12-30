94e717ffb604cfcd6850f6252cfc51fb11e606ee
Allan Yip <papatekken@gmail.com>
2023-12-30 00:29:32 +0000

    add springboot backbone code

---
+++ src/main/java/com/allan/pickfromme/PickfrommeApplication.java
@@ -0,0 +1,13 @@
+package com.allan.pickfromme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickfrommeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickfrommeApplication.class, args);
+	}
+
+}
