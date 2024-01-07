713bbf21c0bac8f2c2380e0d9f36c54f668663b1
Allan Yip <papatekken@gmail.com>
2024-01-07 17:43:54 +0000

    add testing endpoint temporary

--- src/main/java/com/allan/pickfromme/PickfrommeApplication.java
+++ src/main/java/com/allan/pickfromme/PickfrommeApplication.java
@@ -2,12 +2,22 @@ package com.allan.pickfromme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
+
 public class PickfrommeApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PickfrommeApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World";
+	}
 }
